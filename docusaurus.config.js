cfdaac6e909be5519009be4de922cc4dc4620bb5
saji-leoscientific <88245940+saji-leoscientific@users.noreply.github.com>
2021-08-14 11:58:55 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -31,8 +31,8 @@ module.exports = {
       logo: {
         alt: 'Leo Scientific Logo',
         // src: 'img/logo.svg',
-        src: 'img/LeoScientific-logo-light.svg',
-        srcDark: 'img/LeoScientific-logo-dark.png',
+        src: 'img/LeoScientific-logo-light.ico',
+        srcDark: 'img/LeoScientific-logo-dark.ico',
       },
       items: [
         {
