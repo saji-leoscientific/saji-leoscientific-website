c23d837e1fb50dbf040bbe5ef4fd4cc7ac4d8090
saji-leoscientific <88245940+saji-leoscientific@users.noreply.github.com>
2021-08-14 08:28:07 +0530

    change base url to serve page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Leo Scientific',
   tagline: 'View Products with a click',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   // favicon: 'img/favicon.ico',
