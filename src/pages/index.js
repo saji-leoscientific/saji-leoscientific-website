e922118f8248692f43f5c74c65e67f4dbe75f887
Albin-Joseph <thetechtrolltube@gmail.com>
2021-08-20 09:00:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -107,25 +107,7 @@ useEffect(() => {
     // onLoad={trial()}
     >
       
-    { window.onload ?
-    setTimeout(() => {
-     <Layout
-     title={`Hello from ${siteConfig.title}`}
-     description="Description will go into a meta tag in <head />">
-
-       {/* <div className='slideshower'>   */}
-       <Slideshow />
-       {/* </div> */}
-     
-     {/* <p>Basldfksd</p> */}           
-     <HomepageHeader />
-
-     <main>
-       <HomepageFeatures />
-     </main>
-   </Layout>
-    }, 10)
-   :
+  
     <Layout
     title={`Hello from ${siteConfig.title}`}
     description="Description will go into a meta tag in <head />">
@@ -141,7 +123,7 @@ useEffect(() => {
       <HomepageFeatures />
     </main>
   </Layout>
-    }  
+ 
 
     
     {/* <div>{loading ? "loading..." : "hello"}</div>; */}
