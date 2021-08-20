a192dedd387bd518f5e5967cdec78caa6f7fb7c6
Albin-Joseph <thetechtrolltube@gmail.com>
2021-08-20 16:43:57 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,22 +106,14 @@ useEffect(() => {
     //style={{position:"absolute"}}
     // onLoad={trial()}
     >
-   {   
-   useEffect(() => {
-   setTimeout(() => {
-   {/* <div className='slideshower'>   */}
-   <Slideshow />
-      {/* </div> */}
-    }, 10)
-  }, [])
-   }  
       
+  
     <Layout
     title={`Hello from ${siteConfig.title}`}
     description="Description will go into a meta tag in <head />">
-      
+
       {/* <div className='slideshower'>   */}
-      {/* <Slideshow /> */}
+      <Slideshow />
       {/* </div> */}
     
     {/* <p>Basldfksd</p> */}           
