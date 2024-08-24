3bec1390aa466439feb9f7a1f25b679d9dd03334
The-Tech-Troll-Tube <The-Tech-Troll-Tube@users.noreply.github.com>
2024-08-24 12:40:38 +0000

    deploy: ad5cb7e4bb53e82ecf69b161c71e8c5aa9fa9ec6

--- assets/js/535c5f9b.bf4fd9c0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkleo_scientific_website=self.webpackChunkleo_scientific_website||[]).push([[7113],{9352:e=>{e.exports=JSON.parse('{"permalink":"/saji-leoscientific-website/blog/tags/hola","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- assets/js/db6ac6a9.132d0ebd.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkleo_scientific_website=self.webpackChunkleo_scientific_website||[]).push([[3984],{7526:e=>{e.exports=JSON.parse('{"allTagsPath":"/saji-leoscientific-website/blog/tags","name":"hello","count":2,"permalink":"/saji-leoscientific-website/blog/tags/hello"}')}}]);
