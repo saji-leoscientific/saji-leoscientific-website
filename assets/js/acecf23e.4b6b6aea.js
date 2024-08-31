503e7b22877dd6eca9f642cfeba91558879d0eab
The Tech Troll Tube <thetechtrolltube@gmail.com>
2024-08-31 20:55:24 +0530

    Revert "deploy: 76b224ffa900ee3ff1f3b10c3f485e049b6a487e"
    This reverts commit 0ac7cc2200eddf7c4386fad1de985865f7d47002.

---
+++ assets/js/acecf23e.4b6b6aea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkleo_scientific_website=self.webpackChunkleo_scientific_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/saji-leoscientific-website/blog","blogTitle":"Blog","authorsListPath":"/saji-leoscientific-website/blog/authors"}')}}]);
---
+++ assets/js/ec875f20.5b759481.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkleo_scientific_website=self.webpackChunkleo_scientific_website||[]).push([[7095],{8115:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/saji-leoscientific-website/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
